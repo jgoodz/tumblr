c5a58be91cac745258c4e390921c72bfad3a8f48
Justin Goodyer <goodyer@gmail.com>
2017-05-01 21:34:22 -0400

    edit and delete post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,11 +16,33 @@ class PostsController < ApplicationController
 		else
 			render 'new'
 		end
-		
+
 	end
 
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+
+	end
+
+	def destory
+		@post = Post.find(params[:id])
+		@post.destory
+		
+		redirect_to rooth_path
 	end
 
 	private
